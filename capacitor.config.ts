28915b4679d1d94ab4a17d09406126106ea12e79
Melvin Taïder <m.taider@stag.technofuturtic.education>
2023-06-01 16:53:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicDemo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
