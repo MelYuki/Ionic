c14fd974c1d47554c8165e2b48bfc3f71a3f5eee
Melvin Taïder <m.taider@stag.technofuturtic.education>
2023-06-02 12:24:00 +0200

    First commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Product } from './products';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule, FormsModule],
 })
 export class Tab1Page {
   constructor() {}
+
+  productToAdd : string|undefined 
+
+  productList : Product[] = [
+    { name: "Pomme", isChecked: false},
+    { name: "Poire", isChecked: true}
+  ]
 }
